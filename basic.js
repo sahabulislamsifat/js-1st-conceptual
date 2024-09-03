c9ea741be420bbcf54ed83a5031d66ce98e3486a
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-04 01:02:48 +0600

    2nd commit

---
+++ basic.js
@@ -0,0 +1,69 @@
+// console.log(22);
+// console.log('node js');
+// console.log('1.26');
+
+// var Sifat = ("Hello, I am Sifat");
+// console.log(Sifat);
+
+// var me = ("my age 22 running");
+// console.log(me);
+
+// var profession = "I am a student of class BBA"
+// console.log(profession);
+
+// var call = ('call me at Sifat');
+// console.log(call);
+
+
+
+//* Data Type: number, string, boolean, undefine, null, NaN, etc./
+
+
+// var x = 5;
+// var x = "5";
+// var x = true;
+// var i;
+
+// var me = NaN;
+
+// console.log(typeof x);
+// console.log( i);
+// console.log( me);
+
+
+//*Truthy Value => true, "hello", number
+//*Falsy Value => false, "", 0
+
+
+// var number = "abcdefg";
+// console.log(!!number);
+// var x = -0;
+
+
+//* Variable Naming Convention
+
+//*Possible
+// var Sifat;
+// var sahabulSifat; // Camel Case
+// var sahabul_sifat; //Snake Case
+// var SahabulSifat; //Pascel Case
+// var Sifat1  // Number case
+
+//*Not Possible
+// var 98;
+// var 12Sifat
+// var Sifat-HashChangeEvent
+// var @sifat
+// var class // can't use any reserved keyword
+
+
+//* Good Practice
+// var height;
+// var sahabulSifat // Camel Case
+// var sahabul_sifat // Snake Case
+
+
+// var sifat = "my name"
+// var Name = "your name"
+// console.log(sifat);
+// console.log(Name);
---
+++ math.js
@@ -0,0 +1,14 @@
+// console.log(5 + 3);
+
+var x = 6;
+var y = 4;
+var z = 2.22;
+var total = x + y + z;
+// console.log(total);
+
+var sub = x - y;
+var divide = x / z;
+var multi = x * y;
+// console.log(divide);
+var value = divide.toFixed(2)
+console.log(value);
