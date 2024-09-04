fd825ddd28d3967e6fd08bcbc114c3f44fd9c146
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-04 10:17:29 +0600

    3rd commit

--- math.js
+++ math.js
@@ -1,14 +1,57 @@
 // console.log(5 + 3);
 
-var x = 6;
-var y = 4;
-var z = 2.22;
-var total = x + y + z;
+// var x = 6;
+// var y = 4;
+// var z = 2.22;
+// var total = x + y + z;
 // console.log(total);
 
-var sub = x - y;
-var divide = x / z;
-var multi = x * y;
-// console.log(divide);
-var value = divide.toFixed(2)
-console.log(value);
+// var sub = x - y;
+// var multi = x * y;
+// var divide = x / z;
+// console.log(typeof divide);
+// var value = divide.toFixed(4);
+// var value = parseFloat(divide.toFixed(4));
+// console.log(typeof value);
+// console.log(5 + Number(value));
+// console.log( 5 + value)
+
+//* Divide /
+// console.log(10/3)
+// 3^3
+// var divide = 3 ** 3;
+// console.log(divide)
+// var digit = 5;
+// console.log(digit * 5);
+
+//* Modules
+// console.log(10 / 2);
+// var i = 10;
+// console.log(i % 3);
+// console.log(10 % 10)
+// console.log(8 % 10)
+
+//* Shorthand
+// var s = 11;
+// s++
+// s = s + 10;
+// s += 15;
+// console.log(s)
+
+// var x = 20;
+// x--
+// x -= 2;
+// x -= 2;
+// x = x - 1;
+// console.log(x);
+
+// var multi = 10;
+// multi = multi * 2;
+// multi *= 3;
+// console.log(multi)
+
+// var divide = 20;
+// divide /= 2;
+// divide = divide / 3;
+// console.log(divide)
+
---
+++ nan.js
@@ -0,0 +1,37 @@
+// console.log(typeof 5)
+
+//* Not number, can't be transformed into number = true
+//* number or can be transformed into number = false
+
+// var x = "Sifat";
+// console.log(isNaN (x))
+
+// var x = 5;
+// console.log(isNaN(x))
+
+// var x = 5.2;
+// console.log(isNaN(x))
+
+//* Comparison Operator [ ===, !==, =>, <=, >, <]
+
+// x = 5;
+// y = 10;
+// z = 20;
+
+// console.log(x === y)
+// console.log(x !== y)
+// console.log(x > y)
+// console.log(x <= y);
+// console.log(x =>);
+// console.log(x < y);
+// console.log(x > y);
+
+
+//* Difference between == and ===
+// == Check only value
+// === check value and data types
+
+// var x = 5;
+// var y = "5";
+// console.log(x === y)
+// console.log(x == y)
